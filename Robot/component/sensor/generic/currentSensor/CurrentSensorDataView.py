60f776317f89d3a3541de007074f88a30d89a97a
ermu1013 <ermu1013@gmail.com>
2023-10-09 17:59:11 +0200

    fix and implement current streams

--- Robot/component/sensor/generic/currentSensor/CurrentSensorDataView.py
+++ Robot/component/sensor/generic/currentSensor/CurrentSensorDataView.py
@@ -1,3 +1,5 @@
+from tkinter import StringVar
+
 from customtkinter import CTkLabel
 from RoboControl.Robot.Component.generic.distance.DistanceSensor import DistanceSensor
 from RoboControl.Robot.Value.ComponentValue import ComponentValue
@@ -8,37 +10,41 @@ from RoboView.Robot.component.view.SensorDataView import SensorDataView
 class CurrentSensorDataView(SensorDataView):
 
     def __init__(self, root, sensor, settings_key):
-        super().__init__(root, sensor, settings_key, 150, 70)
+        super().__init__(root, sensor, settings_key, 180, 70)
 
         offset_x = 5
         offset_y = 5
         distance_x = 60
         distance_y = 20
 
-        label = CTkLabel(master=self._data_frame, text="actual", width=80, height=15)
-        label.place(x=offset_x, y=offset_y)
-
-        self._actual_label = CTkLabel(self._data_frame, text="", width=80, height=15)
-        self._actual_label.place(x=offset_x + distance_x, y=offset_y)
-
-        label = CTkLabel(self._data_frame, text="max", width=80, height=15)
-        label.place(x=offset_x, y=offset_y + distance_y)
-
-        self._max_label = CTkLabel(self._data_frame, text="", width=80, height=15)
-        self._max_label.place(x=offset_x + distance_x, y=offset_y + distance_y)
-
-        label = CTkLabel(self._data_frame, text="total", width=80, height=15)
-        label.place(x=offset_x, y=offset_y + (distance_y * 2))
-
-        self._total_label = CTkLabel(self._data_frame, text="", width=80, height=15)
-        self._total_label.place(x=offset_x + distance_x, y=offset_y + (distance_y * 2))
-
-        self._actual_value = self._sensor.get_actual_value()
-        self._max_value = self._sensor.get_max_value()
-        self._total_value = self._sensor.get_total_value()
-
+        self._actual = StringVar()
+        CTkLabel(
+            master=self._data_frame, text="actual", width=80, height=15
+        ).place(x=offset_x, y=offset_y)
+        CTkLabel(
+            self._data_frame, textvariable=self._actual, text="", width=80, height=15
+        ).place(x=offset_x + distance_x, y=offset_y)
+        self._actual_value = self._sensor.get_actual()
         self._actual_value.add_listener(self)
+
+        self._max = StringVar()
+        CTkLabel(
+            master=self._data_frame, text="max", width=80, height=15
+        ).place(x=offset_x, y=offset_y + distance_y)
+        CTkLabel(
+            self._data_frame, textvariable=self._max, text="", width=80, height=15
+        ).place(x=offset_x + distance_x, y=offset_y + distance_y)
+        self._max_value = self._sensor.get_max()
         self._max_value.add_listener(self)
+
+        self._total = StringVar()
+        CTkLabel(
+            master=self._data_frame, text="total", width=80, height=15
+        ).place(x=offset_x, y=offset_y + (distance_y * 2))
+        CTkLabel(
+            self._data_frame, textvariable=self._total, text="", width=80, height=15
+        ).place(x=offset_x + distance_x, y=offset_y + (distance_y * 2))
+        self._total_value = self._sensor.get_total()
         self._total_value.add_listener(self)
 
         self.update()
@@ -77,11 +83,11 @@ class CurrentSensorDataView(SensorDataView):
     def component_value_changed(self, component_value: ComponentValue):
         value = str(component_value.get_value()) if component_value.is_valid() else "-"
         if component_value == self._actual_value:
-            self._actual_label.configure(text=f"{value}  c")
+            self._actual.set(f"{value}  c")
         elif component_value == self._max_value:
-            self._max_label.configure(text=f"{value}  c")
+            self._max.set(f"{value}  c")
         elif component_value == self._total_value:
-            self._total_label.configure(text=f"{value}  c")
+            self._total.set(f"{value}  c")
 
     def update(self):
         self.component_value_changed(self._actual_value)
